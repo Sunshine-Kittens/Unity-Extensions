53643339955a3aa14d940f903f224d9a2afae6b2
Luke Andrews <lclandrews@gmail.com>
2026-07-17 11:35:59 +0100

    Added pure delegate binding to update manager to better support static classes.
    - Additional bugs also fixed

--- Runtime/ManagedUpdate/UpdateManager.cs
+++ Runtime/ManagedUpdate/UpdateManager.cs
@@ -7,6 +7,8 @@ namespace UnityEngine.Extension
 {
     public static class UpdateManager
     {
+        public delegate void UpdateMethod();
+        
         private abstract class ManagedUpdateLoopSystemBase<T> : IPlayerLoopSystem where T : IManagedObject
         {
             public EntryPointLocation Location => EntryPointLocation.Before;
@@ -17,36 +19,58 @@ namespace UnityEngine.Extension
 
             private readonly List<T> _deferredRemovals = new List<T>();
             private readonly HashSet<T> _deferredRemovalSet = new HashSet<T>();
+            
+            private readonly List<UpdateMethod> _delegateItems = new List<UpdateMethod>();
+            private readonly Dictionary<UpdateMethod, int> _delegateIndexMap = new Dictionary<UpdateMethod, int>();
+
+            private readonly List<UpdateMethod> _deferredDelegateRemovals = new List<UpdateMethod>();
+            private readonly HashSet<UpdateMethod> _deferredDelegateRemovalSet = new HashSet<UpdateMethod>();
 
             private bool _isIterating = false;
 
-            public ManagedUpdateLoopSystemBase() { }
-            
+            protected ManagedUpdateLoopSystemBase() { }
+
             public bool Contains(T item) => item != null && _indexMap.ContainsKey(item);
-            public int Count => _items.Count;
+            public bool Contains(UpdateMethod item) => item != null && _delegateIndexMap.ContainsKey(item);
+            public int Count => _items.Count + _delegateItems.Count;
             public T[] ToArray() => _items.ToArray();
-            
+
             protected abstract void CallUpdate(T item);
-            
+
             public void Update()
             {
                 _isIterating = true;
-                try
+                
+                for (int i = 0; i < _items.Count; ++i)
                 {
-                    for (int i = 0; i < _items.Count; ++i)
+                    try
                     {
                         T item = _items[i];
                         if (item.Active)
                         {
-                            CallUpdate(item);   
+                            CallUpdate(item);
                         }
                     }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
                 }
-                finally
+
+                for (int i = 0; i < _delegateItems.Count; ++i)
                 {
-                    _isIterating = false;
+                    try
+                    {
+                        _delegateItems[i].Invoke();
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogException(e);
+                    }
                 }
                 
+                _isIterating = false;
+                    
                 if (_deferredRemovals.Count > 0)
                 {
                     for (int i = 0; i < _deferredRemovals.Count; ++i)
@@ -56,12 +80,21 @@ namespace UnityEngine.Extension
                     _deferredRemovals.Clear();
                     _deferredRemovalSet.Clear();
                 }
+
+                if (_deferredDelegateRemovals.Count > 0)
+                {
+                    for (int i = 0; i < _deferredDelegateRemovals.Count; ++i)
+                    {
+                        RemoveStatic(_deferredDelegateRemovals[i]);
+                    }
+                    _deferredDelegateRemovals.Clear();
+                    _deferredDelegateRemovalSet.Clear();
+                }
             }
-            
+
             public void Add(T item)
             {
                 if (item == null) throw new ArgumentNullException(nameof(item));
-                if (_indexMap.ContainsKey(item)) return;
                 
                 if (_deferredRemovalSet.Contains(item))
                 {
@@ -77,19 +110,66 @@ namespace UnityEngine.Extension
                             break;
                         }
                     }
+                    return;
                 }
+
+                if (_indexMap.ContainsKey(item)) return;
+
                 int index = _items.Count;
                 _items.Add(item);
                 _indexMap[item] = index;
             }
-            
+
+            public void AddStatic(UpdateMethod item)
+            {
+                if (item == null) throw new ArgumentNullException(nameof(item));
+
+                if(!item.Method.IsStatic)
+                    throw new InvalidOperationException($"Method {item.Method.Name} is not static");
+                
+                if (_deferredDelegateRemovalSet.Contains(item))
+                {
+                    _deferredDelegateRemovalSet.Remove(item);
+                    int last = _deferredDelegateRemovals.Count - 1;
+                    for (int i = 0; i <= last; ++i)
+                    {
+                        if (_deferredDelegateRemovals[i] == item)
+                        {
+                            if (i != last)
+                                _deferredDelegateRemovals[i] = _deferredDelegateRemovals[last];
+                            _deferredDelegateRemovals.RemoveAt(last);
+                            break;
+                        }
+                    }
+                    return;
+                }
+
+                if (_delegateIndexMap.ContainsKey(item)) return;
+
+                int index = _delegateItems.Count;
+                _delegateItems.Add(item);
+                _delegateIndexMap[item] = index;
+            }
+
             public bool Remove(T item)
             {
                 if (_isIterating)
                     return DeferRemoval(item);
                 return RemoveImmediate(item);
             }
-            
+
+            public bool RemoveStatic(UpdateMethod item)
+            {
+                if (item == null) throw new ArgumentNullException(nameof(item));
+                    
+                if(!item.Method.IsStatic)
+                    throw new InvalidOperationException($"Method {item.Method.Name} is not static");
+                
+                if (_isIterating)
+                    return DeferDelegateRemoval(item);
+                return RemoveDelegateImmediate(item);
+            }
+
             private bool RemoveImmediate(T item)
             {
                 if (item == null) return false;
@@ -97,23 +177,50 @@ namespace UnityEngine.Extension
 
                 int last = _items.Count - 1;
                 T lastItem = _items[last];
-                
+
                 if (index != last)
                 {
                     _items[index] = lastItem;
                     _indexMap[lastItem] = index;
                 }
-                
+
                 _items.RemoveAt(last);
                 _indexMap.Remove(item);
                 return true;
             }
-            
+
             private bool DeferRemoval(T item)
             {
-                if (item == null || !_deferredRemovalSet.Add(item)) 
+                if (item == null || !_deferredRemovalSet.Add(item))
                     return false;
                 _deferredRemovals.Add(item);
+                return true;
+            }
+
+            private bool RemoveDelegateImmediate(UpdateMethod item)
+            {
+                if (item == null) return false;
+                if (!_delegateIndexMap.TryGetValue(item, out int index)) return false;
+
+                int last = _delegateItems.Count - 1;
+                UpdateMethod lastItem = _delegateItems[last];
+
+                if (index != last)
+                {
+                    _delegateItems[index] = lastItem;
+                    _delegateIndexMap[lastItem] = index;
+                }
+
+                _delegateItems.RemoveAt(last);
+                _delegateIndexMap.Remove(item);
+                return true;
+            }
+
+            private bool DeferDelegateRemoval(UpdateMethod item)
+            {
+                if (item == null || !_deferredDelegateRemovalSet.Add(item))
+                    return false;
+                _deferredDelegateRemovals.Add(item);
                 return true;
             }
         }
@@ -156,29 +263,59 @@ namespace UnityEngine.Extension
             _updatablesPlayerLoopSystem.Add(updatable);
         }
 
+        public static void AddStaticUpdatable(UpdateMethod updateAction)
+        {
+            _updatablesPlayerLoopSystem.AddStatic(updateAction);
+        }
+        
         public static void RemoveUpdatable(IUpdatable updatable)
         {
             _updatablesPlayerLoopSystem.Remove(updatable);
         }
 
+        public static void RemoveStaticUpdatable(UpdateMethod updateAction)
+        {
+            _updatablesPlayerLoopSystem.RemoveStatic(updateAction);
+        }
+        
         public static void AddLateUpdatable(ILateUpdatable lateUpdatable)
         {
             _lateUpdatablesPlayerLoopSystem.Add(lateUpdatable);
         }
 
+        public static void AddStaticLateUpdatable(UpdateMethod lateUpdateAction)
+        {
+            _lateUpdatablesPlayerLoopSystem.AddStatic(lateUpdateAction);
+        }
+        
         public static void RemoveLateUpdatable(ILateUpdatable lateUpdatable)
         {
             _lateUpdatablesPlayerLoopSystem.Remove(lateUpdatable);
         }
 
+        public static void RemoveStaticLateUpdatable(UpdateMethod lateUpdateAction)
+        {
+            _lateUpdatablesPlayerLoopSystem.RemoveStatic(lateUpdateAction);
+        }
+        
         public static void AddFixedUpdatable(IFixedUpdatable fixedUpdatable)
         {
             _fixedUpdatablesPlayerLoopSystem.Add(fixedUpdatable);
         }
 
+        public static void AddStaticFixedUpdatable(UpdateMethod fixedUpdateAction)
+        {
+            _fixedUpdatablesPlayerLoopSystem.AddStatic(fixedUpdateAction);
+        }
+        
         public static void RemoveFixedUpdatable(IFixedUpdatable fixedUpdatable)
         {
             _fixedUpdatablesPlayerLoopSystem.Remove(fixedUpdatable);
+        }
+
+        public static void RemoveStaticFixedUpdatable(UpdateMethod fixedUpdateAction)
+        {
+            _fixedUpdatablesPlayerLoopSystem.RemoveStatic(fixedUpdateAction);
         }
     }
 }
